c656b00b7b426f73dbc08e8d5b94d5938a383178
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-09-23 00:49:47 -0300

    fix: manejo de excepciones

--- app/routers/jugadores.py
+++ app/routers/jugadores.py
@@ -27,22 +27,19 @@ router = APIRouter(
 @router.get('/{partida_id:int}/jugadores', response_model=list[JugadorOnCreateResponse])
 async def get_jugadores(partida_id: int, db: Session = Depends(get_db)):
     try:
-        jugadores = crud.get_jugadores(db, partida_id)
+        return crud.get_jugadores(db, partida_id)
     except PartidaNotFoundError as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Partida Not Found")
     except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    return jugadores
 
 @router.post('/{partida_id:int}/jugadores', response_model=JugadorOnCreateResponse)
 async def join_to_partida(partida_id: int, jugador:JugadorData, db: Session = Depends(get_db)):
     try:
-        jugador = crud.create_jugador(db, Jugador(nombre=jugador.nombre, partida_id=partida_id))
+        return crud.create_jugador(db, Jugador(nombre=jugador.nombre, partida_id=partida_id))
     except PartidaNotFoundError as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Partida Not Found")
     except PartidaLlenaError as e:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Partida Full")
     except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    # WARNING: If we leave it like this, then a disconnection could produce "dangling jugadors" (if we don't give a medium for reconnecting)
-    return jugador
--- app/routers/partidas.py
+++ app/routers/partidas.py
@@ -5,8 +5,9 @@ from fastapi import (
 )
 
 from sqlalchemy.orm import Session
-from http import HTTPStatus
+from fastapi import status
 
+from crud.exceptions import PartidaNotFoundError
 import crud.partidas as crud
 from models import Base
 from database import engine, get_db
@@ -21,12 +22,23 @@ router = APIRouter(
 
 @router.get('/', response_model=list[PartidaDetails]) #
 async def get_partidas(db: Session = Depends(get_db)):
-    return crud.get_partidas(db)
+    try:
+        return crud.get_partidas(db)
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.get('/{partida_id:int}', response_model=PartidaDetails)
 async def get_partida_details(partida_id: int, db: Session = Depends(get_db)):
-    return crud.get_partida_details(db, partida_id)
+    try:
+        return crud.get_partida_details(db, partida_id)
+    except PartidaNotFoundError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Partida Not Found")
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @router.post('/', response_model=PartidaDetails)
 async def create_partida(partida: PartidaData, db: Session = Depends(get_db)):
-    return crud.create_partida(db=db, partida=partida)
+    try:
+        return crud.create_partida(db=db, partida=partida)
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
