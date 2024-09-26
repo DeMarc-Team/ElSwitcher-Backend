dfe2718f1c9ecdc3c9e86b0327a2bdf8bcc3d3b7
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-09-26 17:55:49 -0300

    test: test_get_partidas_200 con chequeo para que solo se devuelvan partidas no iniciadas

--- app/tests/test_get_partidas.py
+++ app/tests/test_get_partidas.py
@@ -9,32 +9,34 @@ def test_data():
     # Cargamos en la db de prueba una partida con dos jugadores
     db = TestingSessionLocal()
 
-    partida = Partida(nombre_partida="nombre_partida", nombre_creador="Jugador1", iniciada=False)
+    partida1 = Partida(nombre_partida="partida_no_iniciada", nombre_creador="Jugador1", iniciada=False)
     jugador1 = Jugador(nombre="Jugador1", partida_id=1)
     jugador2 = Jugador(nombre="Jugador2", partida_id=1)
+    partida2 = Partida(nombre_partida="partida_iniciada", nombre_creador="Jugador1", iniciada=True)
 
-    db.add(partida)
+    db.add(partida1)
     db.add(jugador1)
     db.add(jugador2)
+    db.add(partida2)
 
     db.commit()
     db.flush()
     db.close()
 
-    # Cerramos la sesión antes de salir del fixture
-    yield  db # Este es el punto donde se ejecutan las pruebas
+    yield  db
 
-    # Limpiamos la base de datos después de la prueba
     db.query(Jugador).delete()
     db.query(Partida).delete()
     db.commit()
     db.flush()
     db.close()
 
-def test_get_partidas(test_data):
-    response = client.get("partidas") 
+def test_get_partidas_200(test_data):
+    response = client.get("partidas")
     print(f"Response: {response.json()}")
     assert response.status_code == 200 , f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-    assert len(response.json()) == 1 ,  f"Fallo: Se esperaba 1 partida, pero se obtuvo {len(response.json())}"
-    assert response.json()[0]['nombre_partida'] == "nombre_partida"
-    print("Test exitoso")
+    assert len(response.json()) == 1 ,  f"Fallo: Se esperaba solo 1 partida (no iniciada), pero se obtuvo {len(response.json())}"
+    assert response.json()[0]['nombre_partida'] == "partida_no_iniciada", f"Fallo: Se esperaba 'partida_no_iniciada', pero se obtuvo {response.json()[0]['nombre_partida']}"
+    assert response.json()[0]['nombre_creador'] == "Jugador1", f"Fallo: Se esperaba 'Jugador1', pero se obtuvo {response.json()[0]['nombre_creador']}"
+    assert response.json()[0]['iniciada'] == False, f"Fallo: Se esperaba False, pero se obtuvo {response.json()[0]['iniciada']}"
+    #Test exitoso: Se obtuvo la partida no iniciada
