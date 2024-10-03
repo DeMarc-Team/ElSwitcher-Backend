d1075bf85c394df1bd6106fb439a63dd104147c2
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-10-03 15:17:38 -0300

    fix: se elimino delete-ophram de jugadores de Partida

--- app/models.py
+++ app/models.py
@@ -56,7 +56,7 @@ class Partida(Base):
     nombre_creador = mapped_column(String(200), nullable=False)
     iniciada = mapped_column(Boolean, default=False)
     jugadores: Mapped[list[Jugador]] = relationship(
-        'Jugador', back_populates='partidas', cascade="all, delete-orphan", order_by='Jugador.orden', collection_class=ordering_list('orden'))
+        'Jugador', back_populates='partidas', cascade="all", order_by='Jugador.orden', collection_class=ordering_list('orden'))
     @hybrid_property
     def id_creador(self) -> int:
         id_jugador_creador = next(
