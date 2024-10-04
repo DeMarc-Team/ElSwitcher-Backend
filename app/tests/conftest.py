17f6e3b9f527739162288bbab8b21e63dbdfa907
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-10-03 21:21:19 -0300

    refactor: se elimino codifo redundante de conftest.py

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -7,44 +7,14 @@ def setup_db():
     from tests_setup import engine
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-
-    # Creamos la base de datos de prueba
-    from models import Partida, Jugador, CartaFigura, CartaMovimiento
-    from tests_setup import TestingSessionLocal
-
-    db = TestingSessionLocal()
-    try:
-        db.query(CartaFigura).delete()
-        db.query(CartaMovimiento).delete()
-        db.query(Jugador).delete()
-        db.query(Partida).delete()
-        db.commit()
-    except Exception as e:
-        db.rollback()
-        raise e
-    finally:
-        db.close()
     yield
 
 # Eliminamos la base de datos de prueba después de todos los tests
 @pytest.fixture(autouse=True, scope='session')
 def teardown_db():
-    import os
-
-    # Eliminamos la base de datos maindata.db
-
-    # try:
-    #     db_path = os.path.join(os.path.dirname(__file__), '../maindata.db')
-    #     if os.path.exists(db_path):
-    #         os.remove(db_path)
-    #         print(f"El archivo de base de datos {db_path} ha sido eliminado.")
-    #     else:
-    #         print(f"El archivo de base de datos {db_path} no existe.")
-    # except Exception as e:
-    #     print(f"Error al eliminar el archivo de base de datos: {e}")
-
     yield
 
+    import os
     try:
         db_path = os.path.join(os.path.dirname(__file__), 'test.db')
         if os.path.exists(db_path):
