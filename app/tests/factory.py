091dfc64e8e9ba4cd5ce2e9a881f35b1a46c2566
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-10-04 23:27:56 -0300

    feat: se creo un archivo donde tener funciones para modificar la db de test

--- app/tests/factory.py
+++ app/tests/factory.py
@@ -1,37 +1,40 @@
 from sqlalchemy.orm import Session
 from models import Partida, Jugador
 
-
 def crear_partida(db: Session, nombre_partida: str = "Partida", nombre_creador: str = "Creador") -> Partida:
     '''
-    Función para crear una partida
-    Devuelve la partida creada y el jugador creador
+    Función para crear una partida.
+
+    Devuelve la partida creada y el jugador creador.
+
     Valores por defecto:
     - nombre_partida = Partida
     - nombre_creador = Creador
     - tablero = '[[2, 1, 3, 4, 2, 3], [4, 2, 1, 1, 3, 3], [2, 1, 3, 2, 3, 4], [4, 1, 1, 2, 2, 4], [1, 3, 1, 2, 1, 3], [2, 3, 4, 4, 4, 4]]'
     '''
-    nueva_partida = Partida(nombre_partida=nombre_partida,
+    partida = Partida(nombre_partida=nombre_partida,
                             nombre_creador=nombre_creador,
                             iniciada=False,
                             tablero='[[2, 1, 3, 4, 2, 3], [4, 2, 1, 1, 3, 3], [2, 1, 3, 2, 3, 4], [4, 1, 1, 2, 2, 4], [1, 3, 1, 2, 1, 3], [2, 3, 4, 4, 4, 4]]')
     creador = Jugador(nombre=nombre_creador,
-                      partida=nueva_partida,
+                      partidas=partida,
                       es_creador=True,
                       orden=0)
-    nueva_partida.jugadores.append(creador)
+    partida.jugadores.append(creador)
     
     db.add(creador)
-    db.add(nueva_partida)
+    db.add(partida)
     db.commit()
 
-    return nueva_partida, creador
+    return partida, creador
 
 
 def unir_jugadores(db: Session, partida: Partida = "Partida", numero_de_jugadores: int = 1) -> Jugador:
     '''
-    Función para unir jugadores a una partida
-    Devuelve el jugador creado
+    Función para unir jugadores a una partida.
+
+    Devuelve una lista con los jugadores unidos a la partida.
+
     Valores por defecto:
     - partida = Partida
     - numero_de_jugadores = 1
@@ -42,10 +45,27 @@ def unir_jugadores(db: Session, partida: Partida = "Partida", numero_de_jugadore
     assert numero_de_jugadores < 4, "No se pueden unir más de 4 jugadores a la partida"
     assert numero_de_jugadores > 0, "Y el creador? boludito"
 
+    nuevos_jugadores = []
     for i in range(numero_de_jugadores):
-        nuevo_jugador = Jugador(nombre=f"Jugador{i+2}", partida=partida, es_creador=False, orden=len(partida.jugadores))
+        nuevo_jugador = Jugador(nombre=f"Jugador{i+2}", partidas=partida, es_creador=False, orden=len(partida.jugadores))
         db.add(nuevo_jugador)
         partida.jugadores.append(nuevo_jugador)
+        nuevos_jugadores.append(nuevo_jugador)
         db.commit()
 
-    return nuevo_jugador
+    return nuevos_jugadores
+
+def iniciar_partida(db: Session, partida: Partida) -> Partida:
+    '''
+    Función para iniciar una partida.
+
+    Devuelve la partida iniciada.
+    '''
+    assert partida.iniciada == False, "La partida ya ha sido iniciada"
+    assert len(partida.jugadores) > 1, "La partida debe tener al menos 2 jugadores para poder iniciarla"
+    assert len(partida.jugadores) <= 4, "La partida no puede tener más de 4 jugadores"
+
+    partida.iniciada = True
+    db.commit()
+
+    return partida
