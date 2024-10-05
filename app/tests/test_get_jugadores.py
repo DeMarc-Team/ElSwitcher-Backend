b7e738e92683fd6a331e3199fe8a4d2dea7f9839
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-10-04 21:31:47 -0300

    test: get_jugadores estandarizado

--- app/tests/test_get_jugadores.py
+++ app/tests/test_get_jugadores.py
@@ -33,17 +33,20 @@ def test_get_jugadores_200(test_data):
     # Llamamos al endpoint para obtener jugadores de la partida con ID 1 (existente)
     response = client.get("partidas/1/jugadores")
     print(f"Response: {response.json()}")
+
+    # Verificamos que la respuesta sea la esperada
     assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
     assert len(response.json()) == 3, f"Fallo: Se esperaban 3 jugadores, pero se obtuvieron {len(response.json())}"
-    assert response.json()[0]['nombre'] == "Creador", f"Fallo: Se esperaba Creador, pero se obtuvo {response.json()[0]['nombre']}"
-    assert response.json()[1]['nombre'] == "Jugador1", f"Fallo: Se esperaba Jugador1, pero se obtuvo {response.json()[1]['nombre']}"
-    assert response.json()[2]['nombre'] == "Jugador2", f"Fallo: Se esperaba Jugador2, pero se obtuvo {response.json()[2]['nombre']}"
-    #Test exitoso: Se obtuvieron los jugadores de una partida existente
+    respuesta_esperada = [{'nombre': 'Creador', 'id_jugador': 1},
+                          {'nombre': 'Jugador1', 'id_jugador': 2},
+                          {'nombre': 'Jugador2', 'id_jugador': 3}]
+    assert response.json() == respuesta_esperada, f"Fallo: Se esperaba {respuesta_esperada} como respuesta, pero se obtuvo {response.json()}"
 
 def test_get_jugadores_404():
     # Llamamos al endpoint para obtener jugadores de la partida con ID 2 (inexistente)
     response = client.get("partidas/2/jugadores")
     print(f"Response: {response.json()}")
+    
     assert response.status_code == 404, f"Fallo: Se esperaba el estado 404, pero se obtuvo {response.status_code}"
-    assert len(response.json()) == 1, f"Fallo: Se esperaba 1 jugador, pero se obtuvieron {len(response.json())}"
-    #Test exitoso: Se devolvio 404 not found a la peticion de los jugadores de una partida inexistente
+    respuesta_esperada = {'detail': 'Partida con ID 2 no encontrada.'}
+    assert response.json() == respuesta_esperada, f"Fallo: Se esperaba {respuesta_esperada} como respuesta, pero se obtuvo {response.json()}"
