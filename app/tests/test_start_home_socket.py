fd37ce61038f0a0784b707a4f19e76b5628b1039
ArielIvanLeiva <ariel.leiva@mi.unc.edu.ar>
2024-10-05 23:24:07 -0300

    DEM-81 add: Agregada información sobre el test de start_home_socket.

--- app/tests/test_start_home_socket.py
+++ app/tests/test_start_home_socket.py
@@ -2,6 +2,10 @@ from tests_setup import client
 from websockets_manager.ws_home_manager import ws_home_manager
 
 def test_start_home_socket():
+    """
+    Test que prueba el establecimiento y almacenamiento de una conexion por medio de websockets orientada al home.
+    """
+    
     with client.websocket_connect("/partidas/") as websocket:
         num_of_connections = len(ws_home_manager.active_connections)
         
