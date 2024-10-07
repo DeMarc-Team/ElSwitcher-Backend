71437b69b6815ed2dc1e48fec30497519e577541
ArielIvanLeiva <ariel.leiva@mi.unc.edu.ar>
2024-10-07 14:03:20 -0300

    add: Mejorado test del home socket

--- app/tests/test_start_home_socket.py
+++ app/tests/test_start_home_socket.py
@@ -1,5 +1,7 @@
+import pytest
+import mock
 from tests_setup import client
-from websockets_manager.ws_home_manager import ws_home_manager
+from websockets_manager.ws_home_manager import ws_home_manager, MessageType, WsMessage
 
 def test_start_home_socket():
     """
@@ -15,3 +17,35 @@ def test_start_home_socket():
     # Revisamos que la conexion se haya eliminado.
     num_of_connections = len(ws_home_manager.active_connections)
     assert num_of_connections == 0, f"Fallo: Se esperaba que no hubieran conexiones por websockets en el home, pero hay {num_of_connections} conexiones."
+
+
+# Este fixture evita que anyio intente usar trio, y use unicamente la libreria asyncio.
+@pytest.fixture
+def anyio_backend():
+    return 'asyncio'
+
+@pytest.mark.anyio()
+async def test_home_manager_actualizar_partidas():
+    """
+    Test que prueba el funcionamiento unitario del broadcast del home_manager.
+    """
+
+    # Creamos un diccionario de falsos websockets
+    fake_active_connections = {i: mock.AsyncMock() for i in range(0,10)}
+
+    # Modificamos el diccionario de conexiones existentes
+    ws_home_manager.active_connections = fake_active_connections
+
+    # Enviamos un mensaje por broadcast
+    await ws_home_manager.send_actualizar_partidas()
+
+    # Revisamos si se llamaron todos los falsos websockets con el mensaje adecuado, una unica vez.
+    for fake_connection in ws_home_manager.active_connections.items():
+        connection_id, fake_ws = fake_connection
+
+        assert fake_ws.send_text.call_count == 1, f"Fallo: Se esperaba que el websocket de id {connection_id} recibiera un único mensaje."
+        
+        # Revisamos que el mensaje sea el que se corresponde con la especificacion de la api.
+        fake_ws.send_text.assert_called_with(
+            WsMessage(action="actualizar_partidas").json()
+        )
