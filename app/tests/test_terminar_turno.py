48bd8bbdbaedf4112f3701f4dfad6a8c6a50cd84
ArielIvanLeiva <ariel.leiva@mi.unc.edu.ar>
2024-10-10 23:32:45 -0300

    DEM-114 refac: Se refactorizó el test terminar_turno para que utilice el factory y sea más comprensible

--- app/tests/test_terminar_turno.py
+++ app/tests/test_terminar_turno.py
@@ -1,85 +1,60 @@
-from tests_setup import client, TestingSessionLocal
-from models import Partida, Jugador, CartaFigura, CartaMovimiento
-import pytest
-
-def iniciar_partida_de_cuatro(db):
-    nueva_partida = {
-        "nombre_partida": "Partida_nueva",
-        "nombre_creador": "Jugador_nuevo"
-    }
-    response = client.post("/partidas", json=nueva_partida)
-    print(f"Response: {response.json()}")
-
-    # Verificamos que la respuesta sea la esperada
-    assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-    response_data = response.json()
-    assert response_data['nombre_partida'] == "Partida_nueva", f"Fallo: Se obtuvo {response_data['nombre']} como nombre de la partida"
-    assert response_data['nombre_creador'] == "Jugador_nuevo", f"Fallo: Se obtuvo {response_data['nombre_creador']} como nombre del creador de la partida"
-    
-    # Verificamos que la partida se haya creado correctamente en la db
-    partida = db.query(Partida).filter(Partida.id == 1).first()
-    
-    assert partida.nombre_partida == "Partida_nueva", f"Fallo: Se esperaba Partida_nueva como nombre de la partida, pero se obtuvo {partida.nombre_partida}"
-    assert partida.nombre_creador == "Jugador_nuevo", f"Fallo: Se esperaba Jugador_nuevo como nombre del creador de la partida, pero se obtuvo {partida.nombre_creador}"
-    
-    # Añadimos otros jugadores a la partida
-    otros_jugadores = [{"nombre": f"jugador{i}"} for i in range(0,3)]
-    
-    for jugador in otros_jugadores:
-        response = client.post(f'/partidas/{partida.id}/jugadores', json=jugador)
-        assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-    
-    # Iniciamos la partida
-    response = client.put(f"/partidas/{partida.id}")
-    assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-    
-    return partida
-   
-def crear_partida_sin_iniciar(db):
-    nueva_partida = {
-        "nombre_partida": "Partida_nueva",
-        "nombre_creador": "Jugador_nuevo"
-    }
-    response = client.post("/partidas", json=nueva_partida)
-    print(f"Response: {response.json()}")
-
-    # Verificamos que la respuesta sea la esperada
-    assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-    response_data = response.json()
-    assert response_data['nombre_partida'] == "Partida_nueva", f"Fallo: Se obtuvo {response_data['nombre']} como nombre de la partida"
-    assert response_data['nombre_creador'] == "Jugador_nuevo", f"Fallo: Se obtuvo {response_data['nombre_creador']} como nombre del creador de la partida"
-    
-    # Verificamos que la partida se haya creado correctamente en la db
-    partida = db.query(Partida).filter(Partida.id == 1).first()
-    
-    assert partida.nombre_partida == "Partida_nueva", f"Fallo: Se esperaba Partida_nueva como nombre de la partida, pero se obtuvo {partida.nombre_partida}"
-    assert partida.nombre_creador == "Jugador_nuevo", f"Fallo: Se esperaba Jugador_nuevo como nombre del creador de la partida, pero se obtuvo {partida.nombre_creador}"
-    
-    return partida
+from tests_setup import client
+from factory import crear_partida, unir_jugadores, iniciar_partida
 
 def test_terminar_turno(test_db):
-    '''Test sobre el funcionamiento de terminar un turno'''
-    db = test_db
-    partida = iniciar_partida_de_cuatro(db)
+    '''Test que chequea el funcionamiento en el escenario exitoso del endpoint para terminar_turno.'''
+    partida, _ = crear_partida(test_db)
+    unir_jugadores(test_db, partida, numero_de_jugadores=3)
+    iniciar_partida(test_db, partida)
 
-    # Refrescamos el objeto de la partida luego de los cambios en la db
-    db.refresh(partida)
+    jugador_inicial = partida.jugador_del_turno
+    segundo_jugador = partida.jugadores[1]
 
-    
-    # Verificamos que la partida esté iniciada
-    assert partida.iniciada
-    
-    # Obtenemos el id del jugador que tiene el primer turno
-    response = client.get(f'juego/{partida.id}/turno')
-    assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-    id_jugador_inicial = response.json()['id_jugador']
+    # Construyo una copia de los valores que deben permanecer invariantes
+    partida_datos_invariantes = {
+        "id": partida.id,
+        "nombre_partida": partida.nombre_partida,
+        "nombre_creador": partida.nombre_creador,
+        "iniciada": partida.iniciada,
+        "tablero": str(partida.tablero),
+        "id_creador": partida.id_creador
+    }
+
+    # Pasamos el turno
+    response = client.put(f'/juego/{partida.id}/jugadores/{jugador_inicial.id_jugador}/turno')
+    assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}."
+    test_db.commit()
+
+    partida_datos_posteriores = {
+        "id": partida.id,
+        "nombre_partida": partida.nombre_partida,
+        "nombre_creador": partida.nombre_creador,
+        "iniciada": partida.iniciada,
+        "tablero": str(partida.tablero),
+        "id_creador": partida.id_creador
+    }
+
+    # Comprobamos que se mantengan los datos invariantes
+    assert (
+        partida_datos_invariantes == partida_datos_posteriores
+    ), f"Fallo: Se esperaba que no el único dato que se modificara de la partida fuera el orden, pero no es así."
+
+    # Comprobamos que el turno sea del jugaddor correspondiente
+    assert partida.jugador_del_turno.id_jugador == segundo_jugador.id_jugador
+
+def test_vuelta_completa(test_db):
+    '''Test que chequea el funcionamiento de una vuelta completa de turnos.'''
+
+    partida, _ = crear_partida(test_db)
+    unir_jugadores(test_db, partida, numero_de_jugadores=3)
+    iniciar_partida(test_db, partida)
+
+    id_jugador_inicial = partida.jugador_id
     id_jugador_anterior = None
 
     for i in range(0, len(partida.jugadores)):
         # Obtenemos el id del jugador actual
-        response = client.get(f'juego/{partida.id}/turno')
-        assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-        id_jugador_actual = response.json()['id_jugador']
+        id_jugador_actual = partida.jugador_id
 
         # Verificamos que efectivamente se haya cambiador el turno
         assert id_jugador_actual != id_jugador_anterior, f"Fallo: Se esperaba un cambio de turno en el jugador {id_jugador_actual}, pero esto no ocurrió."
@@ -88,94 +63,80 @@ def test_terminar_turno(test_db):
         
         # Terminamos el turno del jugador actual
         response = client.put(f'juego/{partida.id}/jugadores/{id_jugador_actual}/turno')
-        db.commit()
+        assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}."
+        test_db.commit()
 
     # Obtenemos el id del nuevo jugador actual
-    response = client.get(f'juego/{partida.id}/turno')
-    assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-    id_jugador_actual = response.json()['id_jugador']
+    id_jugador_final = partida.jugador_id
         
     # Verificamos que los turnos "hayan dado la vuelta completa"
-    assert id_jugador_inicial == id_jugador_actual
+    assert id_jugador_inicial == id_jugador_final, f"Fallo: Se esperaba que el último jugador fuera el mismo que el inicial, pero eso no ocurrió."
 
 def test_varias_rondas(test_db):
-    '''Test sobre la confiabilidad de una ronda de turnos.'''
-    db = test_db
-    partida = iniciar_partida_de_cuatro(db)
-    
-    # Refrescamos el objeto de la partida luego de los cambios en la db
-    db.refresh(partida)
-    
-    # Verificamos que la partida esté iniciada
-    assert partida.iniciada
-    
-    # Obtenemos el id del jugador que tiene el primer turno
-    response = client.get(f'juego/{partida.id}/turno')
-    assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-    id_jugador_inicial = response.json()['id_jugador']
-    
+    '''Test sobre la confiabilidad de los turnos a lo largo de varias rondas.'''
+    partida, _ = crear_partida(test_db)
+    unir_jugadores(test_db, partida, numero_de_jugadores=3)
+    iniciar_partida(test_db, partida)
+
     orden_de_turnos = []
     id_jugador_anterior = None
     # Hacemos una primera ronda para identificar el orden por id
     for i in range(0, len(partida.jugadores)):
-        
+
         # Obtenemos el id del jugador actual
-        response = client.get(f'juego/{partida.id}/turno')
-        assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-        id_jugador_actual = response.json()['id_jugador']
-        
-        # Verificamos que efectivamente se haya cambiador el turno
+        id_jugador_actual = partida.jugador_id
+
+        # Verificamos que efectivamente se haya cambiado el turno
         assert id_jugador_actual != id_jugador_anterior, f"Fallo: Se esperaba un cambio de turno en el jugador {id_jugador_actual}, pero esto no ocurrió."
-        
+
         orden_de_turnos.append(id_jugador_actual)
         id_jugador_anterior = id_jugador_actual
-        
+
         # Terminamos el turno del jugador actual
         response = client.put(f'juego/{partida.id}/jugadores/{id_jugador_actual}/turno')
-        db.commit()
+        assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}."
+        test_db.commit()
 
-    assert len(orden_de_turnos) == len(partida.jugadores)
+    # Verificamos que se haya pasado por todos los jugadores
+    assert len(set(orden_de_turnos)) == len(partida.jugadores)
+    
     # Hacemos 4 rondas mas para ver si tal orden se mantiene
     for i in range(0, 4 * len(partida.jugadores)):
-        
+
         # Obtenemos el id del jugador actual
-        response = client.get(f'juego/{partida.id}/turno')
-        assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}"
-        id_jugador_actual = response.json()['id_jugador']
-        
-        # Verificamos que efectivamente se haya cambiador el turno
-        assert id_jugador_actual != id_jugador_anterior, f"Fallo: Se esperaba un cambio de turno en el jugador {id_jugador_actual}, pero esto no ocurrió."
-        
+        id_jugador_actual = partida.jugador_id
+
+        # Verificamos que se este siguiendo el mismo orden de antes
         assert orden_de_turnos[i%len(partida.jugadores)] == id_jugador_actual
         id_jugador_anterior = id_jugador_actual
-        
+
         # Terminamos el turno del jugador actual
         response = client.put(f'juego/{partida.id}/jugadores/{id_jugador_actual}/turno')
-        db.commit()
+        test_db.commit()
 
-def test_casos_prohibidos(test_db):
-    '''Test sobre el la confiabilidad de una ronda de turnos.'''
-    db = test_db
-    partida_iniciada = iniciar_partida_de_cuatro(db)
-    partida_sin_iniciar = crear_partida_sin_iniciar(db)
-    
+def test_partida_inexistente_404(test_db):
+    '''Test sobre los mensajes de error ante el envío de terminar turno a una partida inexistente.'''
+
     # Intentamos terminar el turno de una partida inexistente.
     response = client.put(f'juego/{999999}/jugadores/{999999}/turno')
-    db.commit()
     assert response.status_code == 404, f"Fallo: Se esperaba el estado 404, pero se obtuvo {response.status_code}"
-    
+
+def test_partida_no_iniciada_403(test_db):
+    '''Test sobre los mensajes de error ante el envío de terminar turno a una partida no iniciada.'''
+
+    partida, _ = crear_partida(test_db)
+
     # Intentamos terminar el turno de una partida no iniciada.
-    response = client.put(f'juego/{partida_sin_iniciar.id}/jugadores/{999999}/turno')
-    db.commit()
+    response = client.put(f'juego/{partida.id}/jugadores/{1}/turno')
     assert response.status_code == 403, f"Fallo: Se esperaba el estado 403, pero se obtuvo {response.status_code}"
-    
-    # Intentamos terminar el turno de un jugador que no es el que posee el turno (o no existe).
-    response = client.put(f'juego/{partida_iniciada.id}/jugadores/{999999}/turno')
-    db.commit()
+
+def test_partida_no_iniciada_403(test_db):
+    '''Test sobre los mensajes de error ante el envío de terminar turno de un jugador que no posee el turno.'''
+
+    partida, _ = crear_partida(test_db)
+    unir_jugadores(test_db, partida, numero_de_jugadores=3)
+    iniciar_partida(test_db, partida)
+
+    # Intentamos terminar el turno de un jugador que no posee el turno
+    response = client.put(f'juego/{partida.id}/jugadores/{3}/turno')
     assert response.status_code == 403, f"Fallo: Se esperaba el estado 403, pero se obtuvo {response.status_code}"
-    
-    
-    
-    
-    
-    
