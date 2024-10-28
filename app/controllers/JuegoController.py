acb7391e473eab26ec804152a54c6534d3f3f74a
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-10-28 19:59:23 -0300

    refact: se cambio GameController por JuegoController y se usa crud as juego_service en JuegoController como en PartidaController

--- app/controllers/JuegoController.py
+++ app/controllers/JuegoController.py
@@ -1,29 +1,29 @@
-import crud
+import crud as juego_service
 from websockets_manager.ws_partidas_manager import ws_partidas_manager
 from figuras import hallar_todas_las_figuras_en_tablero
 import json
 
-class GameController:
+class JuegoController:
     def __init__(self, db):
         self.db = db
 
     async def get_cartas_figura_jugador(self, partida_id, jugador_id):
-        return crud.partidas.get_cartas_figura_jugador(self.db, partida_id, jugador_id)
+        return juego_service.partidas.get_cartas_figura_jugador(self.db, partida_id, jugador_id)
 
     async def get_movimientos_jugador(self, id_partida, id_jugador):
-        return crud.juego.get_movimientos_jugador(self.db, id_partida, id_jugador)
+        return juego_service.juego.get_movimientos_jugador(self.db, id_partida, id_jugador)
 
     async def get_turno_details(self, partida_id):
-        return crud.juego.get_turno_details(self.db, partida_id)
+        return juego_service.juego.get_turno_details(self.db, partida_id)
 
     async def terminar_turno(self, id_partida, id_jugador):
-        crud.juego.terminar_turno(self.db, id_partida, id_jugador)
+        juego_service.juego.terminar_turno(self.db, id_partida, id_jugador)
         await ws_partidas_manager.send_actualizar_turno(id_partida)
         await ws_partidas_manager.send_actualizar_tablero(id_partida)
 
     async def get_tablero(self, id_partida):
         """Obtiene el tablero de una partida."""
-        tablero = crud.juego.get_tablero(self.db, id_partida)
+        tablero = juego_service.juego.get_tablero(self.db, id_partida)
         tablero_deserializado = json.loads(tablero)
         response = {
             'tablero': tablero_deserializado,
@@ -32,20 +32,20 @@ class GameController:
         return response
 
     async def modificar_casillas(self, id_partida, id_jugador, coordenadas):
-        crud.juego.modificar_casillas(id_partida, id_jugador, coordenadas, self.db)
+        juego_service.juego.modificar_casillas(id_partida, id_jugador, coordenadas, self.db)
         await ws_partidas_manager.send_actualizar_tablero(id_partida)
         await ws_partidas_manager.send_actualizar_cartas_movimiento(id_partida)
 
     async def deshacer_movimiento(self, id_partida):
-        crud.juego.deshacer_movimiento(self.db, id_partida)
+        juego_service.juego.deshacer_movimiento(self.db, id_partida)
         await ws_partidas_manager.send_actualizar_tablero(id_partida)
         await ws_partidas_manager.send_actualizar_cartas_movimiento(id_partida)
 
     async def get_movimientos_parciales(self, id_partida):
-        return crud.juego.get_movimientos_parciales(self.db, id_partida)
+        return juego_service.juego.get_movimientos_parciales(self.db, id_partida)
 
     async def completar_figura_propia(self, id_partida, id_jugador, figura_data):
-        eventos = crud.juego.completar_figura_propia(self.db, id_partida, id_jugador, figura_data)
+        eventos = juego_service.juego.completar_figura_propia(self.db, id_partida, id_jugador, figura_data)
         hay_ganador = eventos.get("hay_ganador")
         if (hay_ganador):
             id_ganador = hay_ganador.get("id_ganador")
--- app/routers/juego.py
+++ app/routers/juego.py
@@ -5,24 +5,23 @@ from fastapi import (
 from sqlalchemy.orm import Session
 from database import engine, get_db
 from models import Base
-from controllers.GameController import GameController
+from controllers.JuegoController import JuegoController
 from schemas import CartaFiguraData, CartaMovimientoData, TurnoDetails, TableroData, CasillasMov, MovimientoParcialData, CompletarFiguraData
 
 Base.metadata.create_all(bind=engine)
 
 router = APIRouter(prefix="/juego")
 
-# FIXME:  Aca se puede inyectar facilmente service y communication_manager
 # Crear una dependencia para el controlador
 async def get_game_controller(db: Session = Depends(get_db)):
-    return GameController(db)
+    return JuegoController(db)
 
 @router.get('/{partida_id:int}/jugadores/{jugador_id:int}/cartas_figura',
             response_model=list[CartaFiguraData],
             summary="Obtener cartas figura de un jugador",
             description="Devuelve las cartas figura de un jugador.",
             tags=["Juego"])
-async def get_cartas_figura_jugador(partida_id: int, jugador_id: int, controller: GameController = Depends(get_game_controller)):
+async def get_cartas_figura_jugador(partida_id: int, jugador_id: int, controller: JuegoController = Depends(get_game_controller)):
     return await controller.get_cartas_figura_jugador(partida_id, jugador_id)
 
 @router.get('/{id_partida:int}/jugadores/{id_jugador:int}/cartas_movimiento',
@@ -30,7 +29,7 @@ async def get_cartas_figura_jugador(partida_id: int, jugador_id: int, controller
             summary="Obtener cartas de movimiento de un jugador",
             description="Devuelve las cartas de movimiento de un jugador.",
             tags=["Juego"])
-async def get_movimientos_jugador(id_partida: int, id_jugador: int, controller: GameController = Depends(get_game_controller)):
+async def get_movimientos_jugador(id_partida: int, id_jugador: int, controller: JuegoController = Depends(get_game_controller)):
     return await controller.get_movimientos_jugador(id_partida, id_jugador)
 
 @router.get('/{id_partida:int}/turno',
@@ -38,46 +37,46 @@ async def get_movimientos_jugador(id_partida: int, id_jugador: int, controller: 
             summary="Obtener la información de quien posee el turno.",
             description="Devuelve el nombre y el identificador de quien posee el turno en una partida.",
             tags=["Juego"])
-async def get_turno_details(id_partida: int, controller: GameController = Depends(get_game_controller)):
+async def get_turno_details(id_partida: int, controller: JuegoController = Depends(get_game_controller)):
     return await controller.get_turno_details(id_partida)
 
 @router.put('/{id_partida:int}/jugadores/{id_jugador:int}/turno',
             summary="Terminar el turno del jugador actual.",
             description="Termina el turno del jugador actual, si es que el id del mismo coincide con el del parámetro.",
             tags=["Juego"])
-async def terminar_turno(id_partida: int, id_jugador: int, controller: GameController = Depends(get_game_controller)):
+async def terminar_turno(id_partida: int, id_jugador: int, controller: JuegoController = Depends(get_game_controller)):
     await controller.terminar_turno(id_partida, id_jugador)
 
 @router.get('/{id_partida:int}/tablero',
             summary='Obetener el tablero del juego',
             response_model=TableroData,
             tags=["Juego"])
-async def get_tablero(id_partida: int, controller: GameController = Depends(get_game_controller)):
+async def get_tablero(id_partida: int, controller: JuegoController = Depends(get_game_controller)):
     return await controller.get_tablero(id_partida)
 
 @router.put('/{id_partida}/jugadores/{id_jugador}/tablero/casilla',
             summary="Jugar carta movimiento.",
             description="Modificar el tablero según las coordenadas de las fichas que envía el jugador del turno actual.",
             tags=["Juego"])
-async def modificar_casillas(id_partida: int, id_jugador: int, coordenadas: CasillasMov, controller: GameController = Depends(get_game_controller)):
+async def modificar_casillas(id_partida: int, id_jugador: int, coordenadas: CasillasMov, controller: JuegoController = Depends(get_game_controller)):
     await controller.modificar_casillas(id_partida, id_jugador, coordenadas)
 
 @router.delete('/{id_partida}/jugadores/{id_jugador}/mov-parciales',
                summary="Eliminar el último movimiento parcial de un jugador.",
                tags=["Juego"])
-async def deshacer_movimiento(id_partida: int, id_jugador: int, controller: GameController = Depends(get_game_controller)):
+async def deshacer_movimiento(id_partida: int, id_jugador: int, controller: JuegoController = Depends(get_game_controller)):
     await controller.deshacer_movimiento(id_partida)
 
 @router.get('/{id_partida}/jugadores/{id_jugador}/mov-parciales',
                summary="Obtiene el stack de los movimientos parciales",
                response_model=list[MovimientoParcialData],
                tags=["Juego"])
-async def get_movimientos_parciales(id_partida: int, id_jugador: int, controller: GameController = Depends(get_game_controller)):
+async def get_movimientos_parciales(id_partida: int, id_jugador: int, controller: JuegoController = Depends(get_game_controller)):
     return await controller.get_movimientos_parciales(id_partida)
 
 @router.put('/{id_partida:int}/jugadores/{id_jugador:int}/tablero/figura',
             summary="Completar figura propia",
             description="Utiliza la carta de figura especificada a partir de la existencia de la figura en las coordenadas que se pasaron.",
             tags=["Juego"])
-async def completar_figura_propia(id_partida: int, id_jugador: int, figura_data: CompletarFiguraData, controller: GameController = Depends(get_game_controller)):
+async def completar_figura_propia(id_partida: int, id_jugador: int, figura_data: CompletarFiguraData, controller: JuegoController = Depends(get_game_controller)):
     await controller.completar_figura_propia(id_partida, id_jugador, figura_data)
