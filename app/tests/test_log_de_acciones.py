e749b3ee196ad6057ff65c6f156fcde5ed5f0166
emiemiemi <emijoaco_pereyra@hotmail.com>
2024-11-10 15:39:19 -0300

    DEM-133_feature: Tests mas completos de la funcionalidad de log de acciones

--- app/tests/test_log_de_acciones.py
+++ app/tests/test_log_de_acciones.py
@@ -3,6 +3,8 @@ from tools import capturar_metadata as capturar, comparar_capturas, get_all_tabl
 from test_endpoint_jugar_carta_movimiento import agregar_m1_a_los_inventarios, jugar_carta_m1
 from test_bloquear_figura import bloqueo_generico_test
 from verifications import check_response
+from websockets_manager.ws_partidas_manager import ACTUALIZAR_TURNO, ACTUALIZAR_TABLERO, SINCRONIZAR_TURNO
+import mock
 
 def test_log_de_movimiento(client, test_db, test_ws_broadcast_messages):
     test_ws_broadcast_messages['partidas'] = [
@@ -13,15 +15,17 @@ def test_log_de_movimiento(client, test_db, test_ws_broadcast_messages):
     from test_endpoint_jugar_carta_movimiento import test_hacer_movimiento_200
     suprimir_errores_ajenos(test=test_hacer_movimiento_200, client=client, test_db=test_db, test_ws_broadcast_messages=test_ws_broadcast_messages)
 
-# def test_log_deshacer_movimiento(client, test_db, test_ws_broadcast_messages):
-#     test_ws_broadcast_messages['partidas'] = [
-#         {'partida_id': 1, 'message': {'action': 'actualizar_tablero', 'data': None}},
-#         {'partida_id': 1, 'message': {'action': 'actualizar_cartas_movimiento', 'data': None}},
-#         {'partida_id': 1, 'message': {'action': 'sincronizar_mensaje', 'data': {'message': 'Creador: Ha realizado un movimiento', 'id_jugador': 1, 'type_message': 'ACTION'}}},
-#         {'partida_id': 1, 'message': {'action': 'sincronizar_mensaje', 'data': {'message': 'Creador: Ha deshecho su último movimiento', 'id_jugador': 1, 'type_message': 'ACTION'}}},
-#     ]
-#     from test_cancelar_movimiento_parcial import test_cancelar_movimiento_parcial
-#     suprimir_errores_ajenos(test=test_cancelar_movimiento_parcial, client=client, test_db=test_db, test_ws_broadcast_messages=test_ws_broadcast_messages)
+def test_log_deshacer_movimiento(client, test_db, test_ws_broadcast_messages):
+    test_ws_broadcast_messages['partidas'] = [
+        {'partida_id': 1, 'message': {'action': 'actualizar_tablero', 'data': None}},
+        {'partida_id': 1, 'message': {'action': 'actualizar_tablero', 'data': None}},
+        {'partida_id': 1, 'message': {'action': 'actualizar_cartas_movimiento', 'data': None}},
+        {'partida_id': 1, 'message': {'action': 'actualizar_cartas_movimiento', 'data': None}},
+        {'partida_id': 1, 'message': {'action': 'sincronizar_mensaje', 'data': {'message': 'Creador: Ha realizado un movimiento', 'id_jugador': 1, 'type_message': 'ACTION'}}},
+        {'partida_id': 1, 'message': {'action': 'sincronizar_mensaje', 'data': {'message': 'Creador: Ha deshecho su último movimiento', 'id_jugador': 1, 'type_message': 'ACTION'}}},
+    ]
+    from test_cancelar_movimiento_parcial import test_cancelar_movimiento_parcial
+    suprimir_errores_ajenos(test=test_cancelar_movimiento_parcial, client=client, test_db=test_db, test_ws_broadcast_messages=test_ws_broadcast_messages)
     
 def test_log_bloqueo(client, test_db, test_ws_broadcast_messages):
     test_ws_broadcast_messages['partidas'] = [
@@ -42,6 +46,35 @@ def test_log_completar_figura_propia(client, test_db, test_ws_broadcast_messages
     from test_completar_figura_propia import test_usar_figura_propia
     suprimir_errores_ajenos(test=test_usar_figura_propia, client=client, test_db=test_db, test_ws_broadcast_messages=test_ws_broadcast_messages)
 
+def test_log_abandonar_partida(client, test_db, test_ws_broadcast_messages, test_ws_counts):
+    from test_abandonar_partida import test_abandonar_partida_en_el_turno_200 # El test ya se realiza necesariamente en el otro archivo
+    suprimir_errores_ajenos(test=test_abandonar_partida_en_el_turno_200, client=client, test_db=test_db, test_ws_broadcast_messages=test_ws_broadcast_messages)
+
+def test_log_fin_de_turno(client, test_db, test_ws_broadcast_messages, test_ws_counts):
+    with mock.patch('crud.TemporizadorTurno.TemporizadorTurno.get_gmt_zulu_time', return_value="esto esta mockeado"):
+        test_ws_broadcast_messages['partidas'] = [
+            {'partida_id': 1, 'message': {'action': 'sincronizar_mensaje', 'data': {'message': 'El turno de Creador: Ha finalizado', 'id_jugador': 1, 'type_message': 'ACTION'}}},
+            {'partida_id': 1, 'message': {'action': 'actualizar_turno', 'data': None}},
+            {'partida_id': 1, 'message': {'action': 'actualizar_tablero', 'data': None}},
+            {'partida_id': 1, 'message': {'action': 'sincronizar_turno', 'data': {'duracion': 120, 'inicio': 'esto esta mockeado'}}}
+        ]
+
+        partida, _ = crear_partida(test_db)
+        unir_jugadores(test_db, partida, numero_de_jugadores=3)
+        iniciar_partida(test_db, partida)
+        jugador_inicial = partida.jugador_del_turno
+        segundo_jugador = partida.jugadores[1]
+
+        # Pasamos el turno
+        response = client.put(test_db, f'/juego/{partida.id}/jugadores/{jugador_inicial.id_jugador}/turno')
+        assert response.status_code == 200, f"Fallo: Se esperaba el estado 200, pero se obtuvo {response.status_code}."
+
+        # Ponemos cuantas veces se espera que se envie cada mensaje de ws
+        test_ws_counts[ACTUALIZAR_TURNO] = 1
+        test_ws_counts[ACTUALIZAR_TABLERO] = 1
+        test_ws_counts[SINCRONIZAR_TURNO] = 1
+
+
 def suprimir_errores_ajenos(test, client, test_db, test_ws_broadcast_messages):
     """
         Al llamar a otros test para reutilizar la situación para contar
@@ -53,4 +86,3 @@ def suprimir_errores_ajenos(test, client, test_db, test_ws_broadcast_messages):
         test(client, test_db, test_ws_broadcast_messages)
     except Exception as e:
         pass
-    
